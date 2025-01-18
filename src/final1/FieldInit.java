0eb46c57c2e46b2df34053ae4aab93d35f6fb6bd
DESKTOP-72C6IBH\keji96 <keji@eco.co.kr>
2025-01-18 20:00:59 +0900

    final 변수와 상수1

---
+++ src/final1/FieldInit.java
@@ -0,0 +1,11 @@
+package final1;
+
+//final 필드 - 필드 초기화
+public class FieldInit {
+    static final int CONST_VALUE = 10;
+    final int value = 10;
+
+//    public FieldInit(int value) {
+//        this.value = value;
+//    }
+}
