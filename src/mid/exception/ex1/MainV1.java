9560830eea083c2ed3bc533c32f0742b1893d0eb
DESKTOP-2V14N72\user <keji@eco.co.kr>
2025-03-14 14:53:50 +0900

    예외 처리가 필요한 이유3 - 반환 값으로 예외 처리

--- src/mid/exception/ex1/MainV1.java
+++ src/mid/exception/ex1/MainV1.java
@@ -3,23 +3,22 @@ package mid.exception.ex1;
 import java.util.Scanner;
 
 public class MainV1 {
+
     public static void main(String[] args) {
+        //NetworkServiceV1_1 networkService = new NetworkServiceV1_1();
+        NetworkServiceV1_2 networkService = new NetworkServiceV1_2();
+        //NetworkServiceV1_3 networkService = new NetworkServiceV1_3();
 
-        NetworkServiceV1_1 networkService = new NetworkServiceV1_1();
         Scanner scanner = new Scanner(System.in);
-
-        while(true){
+        while (true) {
             System.out.print("전송할 문자: ");
             String input = scanner.nextLine();
-
-            if(input.equals("exit")){
+            if (input.equals("exit")) {
                 break;
             }
-
             networkService.sendMessage(input);
             System.out.println();
         }
-
         System.out.println("프로그램을 정상 종료합니다.");
     }
 }
