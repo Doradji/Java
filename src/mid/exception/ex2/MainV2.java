bb81cb3a9c90c34ae9a0e70588d3d9dd69448163
DESKTOP-2V14N72\user <keji@eco.co.kr>
2025-03-17 11:21:13 +0900

    예외 복구

--- src/mid/exception/ex2/MainV2.java
+++ src/mid/exception/ex2/MainV2.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class MainV2 {
     public static void main(String[] args) throws NetworkClientExceptionV2{
-        NetworkServiceV2_1 newtworkService = new NetworkServiceV2_1();
+        //NetworkServiceV2_1 newtworkService = new NetworkServiceV2_1();
+        NetworkServiceV2_2 newtworkService = new NetworkServiceV2_2();
 
         Scanner scanner = new Scanner(System.in);
         while(true){
