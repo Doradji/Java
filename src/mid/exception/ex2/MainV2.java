bf6830b689b5a3a3160a81849b0238fc6c3e2186
DESKTOP-2V14N72\user <keji@eco.co.kr>
2025-03-17 16:03:32 +0900

    리소스 반환 문제 / finally

--- src/mid/exception/ex2/MainV2.java
+++ src/mid/exception/ex2/MainV2.java
@@ -6,7 +6,9 @@ public class MainV2 {
     public static void main(String[] args) throws NetworkClientExceptionV2{
         //NetworkServiceV2_1 newtworkService = new NetworkServiceV2_1();
         //NetworkServiceV2_2 newtworkService = new NetworkServiceV2_2();
-        NetworkServiceV2_3 newtworkService = new NetworkServiceV2_3();
+        //NetworkServiceV2_3 newtworkService = new NetworkServiceV2_3();
+        //NetworkServiceV2_4 newtworkService = new NetworkServiceV2_4();
+        NetworkServiceV2_5 newtworkService = new NetworkServiceV2_5();
 
         Scanner scanner = new Scanner(System.in);
         while(true){
