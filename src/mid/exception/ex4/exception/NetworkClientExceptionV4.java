caf877a1e690caaf7de7a01056d3115b9679674b
DESKTOP-2V14N72\user <keji@eco.co.kr>
2025-03-18 13:57:32 +0900

    실무 예외 처리 방안2 - 구현

---
+++ src/mid/exception/ex4/exception/NetworkClientExceptionV4.java
@@ -0,0 +1,8 @@
+package mid.exception.ex4.exception;
+
+public class NetworkClientExceptionV4 extends RuntimeException{
+
+    public NetworkClientExceptionV4(String message) {
+        super(message);
+    }
+}
