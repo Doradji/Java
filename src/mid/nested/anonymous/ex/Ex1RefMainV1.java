0c49450db1f350bc2f2dd3937c96fbc83b878ec7
DESKTOP-2V14N72\user <keji@eco.co.kr>
2025-05-14 10:39:22 +0900

    익명 클래스 활용2

---
+++ src/mid/nested/anonymous/ex/Ex1RefMainV1.java
@@ -0,0 +1,36 @@
+package mid.nested.anonymous.ex;
+
+public class Ex1RefMainV1 {
+
+    public static void hello(Process process) {
+        System.out.println("프로그램 시작");
+
+        // 코드 조작 시작
+        process.run();
+        // 코드 조작 종료
+
+        System.out.println("프로그램 종료");
+    }
+
+    static class Dice implements Process {
+        @Override
+        public void run() {
+            int randomValue = new java.util.Random().nextInt(6) + 1;
+            System.out.println("주사위 = " + randomValue);
+        }
+    }
+
+    static class Sum implements Process {
+        @Override
+        public void run() {
+            for (int i = 1; i <= 3; i++) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        hello(new Dice());
+        hello(new Sum());
+    }
+}
