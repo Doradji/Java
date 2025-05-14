8ba583574f32a1c061eabec1a8789e89666f7435
DESKTOP-2V14N72\user <keji@eco.co.kr>
2025-05-14 16:20:16 +0900

    다형성을 통한 중복 해결 시도

---
+++ src/mid2/generic/ex1/BoxMain2.java
@@ -0,0 +1,21 @@
+package mid2.generic.ex1;
+
+public class BoxMain2 {
+    public static void main(String[] args) {
+        ObjectBox integerBox = new ObjectBox();
+        integerBox.set(10);
+        Integer integer = (Integer) integerBox.get(); //Object -> Integer 캐스팅
+        System.out.println("integer = " + integer);
+
+        ObjectBox stringBox = new ObjectBox();
+        stringBox.set("Hello");
+        String str = (String) stringBox.get(); //Object -> String 캐스팅
+        System.out.println("str = " + str);
+
+        // 잘못된 타입의 인수 전달시
+        integerBox.set("문자100");
+        Integer result = (Integer) integerBox.get(); //ClassCastException 발생
+        System.out.println("result = " + result);
+
+    }
+}
